27aa46df43ed86480b39ae551283d44e2bb2b7d4
bryantobing12 <bryantobing0@gmail.com>
2020-05-06 17:45:42 +0700

    origin master

--- aset/js/view/main.js
+++
@@ -1,8 +0,0 @@
-import "../component/card-data.js";
-import "../data/data-world.js";
-import "../data/data-indonesia.js";
-
-
-
-
-
--- src/script/data-source.js
+++
@@ -1,4 +0,0 @@
-function main() {
-    const urlIndonesia = "https://api.kawalcorona.com/indonesia";
-    console.log(urlIndonesia);
-}
